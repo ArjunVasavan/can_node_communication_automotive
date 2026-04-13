c605f8ef462c827b7be362f39e66bafb7f2c8faf
Arjun Vasavan <vasavanarjun@gmail.com>
2026-04-13 23:47:15 +0530

    refactor(ECU1_Sensor): commented for can transrecieving

--- ECU1.X/ECU1_Sensor.c
+++ ECU1.X/ECU1_Sensor.c
@@ -28,26 +28,24 @@ uint16_t get_speed()
 
    can_transmit(SPEED_MSG_ID ,(const uint8_t*)str,2);
    __delay_ms(20);
-   can_receive(&msg_id,rx1_data,(uint8_t*)&len1);
-   rx1_data[len1] = '\0';
 
-
-   if(msg_id == SPEED_MSG_ID) {
-       clcd_print((const uint8_t*)"SPEED", LINE1(0));
-       clcd_print(rx1_data, LINE2(0));
-   }
+   
+   // TODO: CAN comment
+   //
+   // can_receive(&msg_id,rx1_data,(uint8_t*)&len1);
+   // rx1_data[len1] = '\0';
+   //
+   // if(msg_id == SPEED_MSG_ID) {
+   //     clcd_print((const uint8_t*)"SPEED", LINE1(0));
+   //     clcd_print(rx1_data, LINE2(0));
+   // }
 
    return speed;
 }
 
-unsigned char get_gear_pos()
-{
+unsigned char get_gear_pos() {
 
     // Implement the gear function
-
-    /* NOTE: STATE_CHANGE
-     * state change doesnt impact anything we can send void also
-     */
     int key=read_switches(STATE_CHANGE);
     if(key== 1) {
         if(ind<7) {
@@ -61,25 +59,23 @@ unsigned char get_gear_pos()
         ind=8;
     }
 
-    /* NOTE: For ECU3 CAN Connection
-     * comment from 
-     * __delay_ms(20)
-     * to 
-     *
-     */
-
     can_transmit(GEAR_MSG_ID,(const uint8_t*)gear[ind],2);
-    __delay_ms(20);
-    can_receive(&gear_id,rx2_data,(uint8_t*)&len2);
-    rx2_data[len2] = '\0';
-    if(gear_id == GEAR_MSG_ID)
-    {
-        clcd_print((const unsigned char*)"GEAR", LINE1(9));
-        clcd_putch(rx2_data[0], LINE2(9));
-        clcd_putch(rx2_data[1], LINE2(10));
-    }
+    __delay_ms(80);
 
-    // till here
+    // TODO: When using CAN for ECU3 comment ↓
+
+
+    // can_receive(&gear_id,rx2_data,(uint8_t*)&len2);
+    // rx2_data[len2] = '\0';
+    // if(gear_id == GEAR_MSG_ID) {
+    //     clcd_print((const unsigned char*)"GEAR", LINE1(9));
+    //     clcd_putch(rx2_data[0], LINE2(9));
+    //     clcd_putch(rx2_data[1], LINE2(10));
+    // }
+
+
+    // TODO: comment till here ↑
+
 
     return gear[ind][0];
 
